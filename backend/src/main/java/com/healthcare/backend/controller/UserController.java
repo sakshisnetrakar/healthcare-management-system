d155b6d6ab0395c82c1fc687062cb6d8e04e32db
sakshisnetrakar <sakshisnetrakar@gmail.com>
2026-08-13 23:45:26 +0530

    Added method level annotation for role based access over user controller

--- backend/src/main/java/com/healthcare/backend/controller/UserController.java
+++ backend/src/main/java/com/healthcare/backend/controller/UserController.java
@@ -9,7 +9,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.security.access.prepost.PreAuthorize;
 import java.util.List;
 
 @RestController
@@ -25,18 +25,21 @@ public class UserController {
         return userService.registerUser(dto);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
     public List<UserResponseDTO> getAllUsers() {
 
         return userService.getAllUsers();
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{id}")
     public UserResponseDTO getUserById(@PathVariable Long id) {
 
         return userService.getUserById(id);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteUser(@PathVariable Long id) {
 
@@ -45,6 +48,7 @@ public class UserController {
         return ResponseEntity.ok("User deleted successfully");
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<UserResponseDTO> updateUser(
         @PathVariable Long id,
